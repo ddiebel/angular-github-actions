1ce06cdde6dd57e20e061bcd059b8705f65b7b07
dirk diebel <818141+ddiebel@users.noreply.github.com>
2020-05-17 21:54:46 +0200

    demo2 component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Demo2Component } from './demo2/demo2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Demo2Component
   ],
   imports: [
     BrowserModule,
